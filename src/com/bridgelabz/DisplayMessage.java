c97fe57ac34dca7fd8288be00b54ec3b29a8f4cb
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 01:04:30 +0530

    Update DisplayMessage.java

--- src/com/bridgelabz/DisplayMessage.java
+++ src/com/bridgelabz/DisplayMessage.java
@@ -1,11 +1,21 @@
 package com.bridgelabz;
 
 public class DisplayMessage {
+    /**
+    *This Method Created For Print Display Message
+    */
     static void m1(){
         System.out.println("Display Message");
     }
+    /**
+    *This Method Created For Calling m1 Method
+    */
 
     public static void main(String[] args) {
+    /**
+    * Calling m1 Method
+    */
+        
 
         m1();
     }
