01ccb118bdfeb6e0d458c1139fbea9d779664b8b
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 01:08:12 +0530

    Update GetNameInCommandLine.java

--- src/com/bridgelabz/GetNameInCommandLine.java
+++ src/com/bridgelabz/GetNameInCommandLine.java
@@ -1,7 +1,13 @@
 package com.bridgelabz;
 
 public class GetNameInCommandLine {
+    /**
+    *This Method Created For Print the Name which Is pass in CommandLine Arguments
+    */
     public static void main(String[] args) {
+    /**
+    *Print The Name
+    */
 
 
         System.out.println(args[0]);
