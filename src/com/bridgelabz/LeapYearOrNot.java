1837798a9990fb3fc4b0241aca088cf503c144ff
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 01:12:27 +0530

    Update LeapYearOrNot.java

--- src/com/bridgelabz/LeapYearOrNot.java
+++ src/com/bridgelabz/LeapYearOrNot.java
@@ -3,16 +3,28 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class LeapYearOrNot {
+    /**
+    *This Method Created For Checking The Year Is Leap Year Or Not a Leap Year
+    */
     static void CheckLeapYear(){
-        int year=1852;
+    /**
+    *Enter Year For Checking Given Year Is Leap Year Or Not a Leap Year
+    */   
+        int year;
         Scanner sc = new Scanner(System.in);
         year = sc.nextInt();
+    /**
+    *Using If-else condition For Check year is leap Year Or Not Leap Year
+    */
         if (((year % 4 ==0)&& (year % 100 != 0)) || (year % 400 ==0))
             System.out.println("Leap Year");
         else
             System.out.println("Not Leap Year");
 
     }
+    /**
+    *This Method Created For calling checkLeapYear Method 
+    */
 
     public static void main(String[] args){
         System.out.println("Checking Leap Year or not");
