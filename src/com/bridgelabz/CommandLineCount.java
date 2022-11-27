020dc0afff9ff2a63ca5335f9804cf6151497dcb
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 00:48:38 +0530

    Update CommandLineCount.java

--- src/com/bridgelabz/CommandLineCount.java
+++ src/com/bridgelabz/CommandLineCount.java
@@ -1,15 +1,27 @@
 package com.bridgelabz;
 
 public class CommandLineCount {
+    /**
+    *This Method is Created For Sum Of Arguments which is Pass in Commandline 
+    */
     public static void main(String[] args) {
+    /**
+    * Pass Two Values  in  CommandLine Arguments 
+    */
 
         System.out.println(args[0]);
         System.out.println(args[1]);
 
         int X = Integer.parseInt(args [0]);
         int Y = Integer.parseInt(args [1]);
+    /**
+    *Sum of Two Integers x and Y
+    */
 
         int sum = (X+Y);
+     /**
+    *Print The Sum Of Two Numbers
+    */
         
         System.out.println(sum);
     }
