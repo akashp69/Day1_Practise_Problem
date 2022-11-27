006f6d5698ee487a1ca039349d132f6c849007ad
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 00:57:53 +0530

    Update DemoStaticMethod.java

--- src/com/bridgelabz/DemoStaticMethod.java
+++ src/com/bridgelabz/DemoStaticMethod.java
@@ -1,13 +1,24 @@
 package com.bridgelabz;
+ 
 
 public class DemoStaticMethod {
     static int staticvar;
+  /**
+  *This Method is Created For Give Demo Of Static Method
+  */
 
     static void m1(){
         System.out.println("m1 Method");
     }
+  /**
+  *This Method is Created For Call the Method And Static Variable
+  */
 
     public static void main(String[] args) {
+  /**
+  *Output is Static Variable And m1 Method  
+  */
+   
 
         System.out.println(staticvar);
         m1();
